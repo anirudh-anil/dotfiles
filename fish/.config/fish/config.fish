d68b1ba7d575353dfe09939a9b49c45e6969dbb2
Anirudh J Anil <anirudhkunduthode@gmail.com>
2026-06-20 17:32:32 +0530

    fix prompt color and comment out unnecessary configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,13 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set fish_greeting
-
-alias ls="/var/home/anirudh/.bin/eza -l --icons"
+set fish_color_command blue
+alias ls="eza -l --icons"
 #alias wifi="sudo iw dev wlo1 scan > /dev/zero"
 alias q="exit"
 # alias mpd="toolbox run -c fedora mpd"
 alias c="clear"
-alias task="toolbox run -c fedora task"
-alias fzf="toolbox run -c fedora fzf"
-alias wezterm="flatpak run org.wezfurlong.wezterm"
+# alias task="toolbox run -c fedora task"
+# alias fzf="toolbox run -c fedora fzf"
+# alias wezterm="flatpak run org.wezfurlong.wezterm"
 export EDITOR='nvim'
