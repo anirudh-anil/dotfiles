07d473cf65fc9df395a6915deba1fce93fc359ad
Anirudh J Anil <anirudhkunduthode@gmail.com>
2026-09-21 11:41:08 +0530

    update configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,11 +12,6 @@ if status is-interactive
         enable_transience
     end
     
-    # Colors
-    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
-    end
-
     # Aliases
     # kitty doesn't clear properly so we need to do this weird printing
     alias clear "printf '\033[2J\033[3J\033[1;1H'"
